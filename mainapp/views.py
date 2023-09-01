8288d5123d238aa5c1469a595981c20403e11ff8
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2023-09-01 07:54:08 +0630

    'model'

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+     persons = Person.objects.all()
+     context = {
+          'persons':persons
+     }
+     return render(request, 'index.html', context)
